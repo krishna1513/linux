82ba2acf570eb221add0267ad40ddc49c3b0c297
krishna <nmuraleekrishnan@gmail.com>
2021-06-25 15:34:10 +0530

    add one cal class

--- sample.cpp
+++ sample.cpp
@@ -8,8 +8,28 @@ void print(string str)
 	cout<<str<<"\n";	
 }
 
+class test
+{
+	public:
+		int a,b;
+	public:
+		test(int n,int m)
+		{
+			a = n;
+			b = m;
+		}
+		void print_sum()
+		{
+			cout<<"added a+b = "<<a+b<<"\n";
+		}		
+};
+
 int main()
 {
 	print("jai jai ganesh deva 1513 108");
+
+	test t(4,8);
+	t.print_sum();
+
 	return 0;
 }
